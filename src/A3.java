3f004b2a96142660c7fb57d4adb6f5777df17ba8
Ademo277 <ademo277@mtroyal.ca>
2023-11-18 19:27:21 -0700

    Almost done, saveResult is a temporary method we need to delete it later

--- src/A3.java
+++ src/A3.java
@@ -21,7 +21,6 @@ public class A3 {
 	 */
 	private BST<Token> wordsByNaturalOrder = new BST<Token>();
 	private BST<Token> wordsByFreqDesc = new BST<Token>(Token.CompFreqDesc);
-//	private BST<Token> wordsByFreqAsec = new BST<Token>(Token.CompFreqAsc);
 	private BST<Token> wordsByLength = new BST<Token>(Token.CompLengthDesc);
 
 	// 103 stopwords in list
@@ -37,7 +36,7 @@ public class A3 {
 	private int wordCount = 0;
 	private int stopWordCount = 0;
 
-	private final String FILE_PATH = "res/input1.txt";
+	private final String FILE_PATH = "res/input3.txt";
 //    private Scanner scanner = new Scanner(System.in);
 
 	/**
@@ -80,7 +79,7 @@ public class A3 {
 	}
 
 	/**
-	 * Creates the frequency and length lists
+	 * Creates the frequency and length trees
 	 * 
 	 */
 	private void createFreqLists() {
@@ -90,7 +89,7 @@ public class A3 {
 		Iterator<Token> itrNatural = wordsByNaturalOrder.iterator();
 
 		// Make the list
-		while (itrNatural.hasNext() && counter < 10) {
+		while (itrNatural.hasNext()) {
 			Token data = itrNatural.next();
 			
 			if (data.getCount() > 2) {
@@ -142,7 +141,10 @@ public class A3 {
 	 * @return int - optimal height of tree
 	 */
 	private int optHeight(int n) {
-		return (int) (Math.log(n) / Math.log(2));
+		if(n != 0) {
+			return (int) (Math.log(n) / Math.log(2));
+		}
+		return -1;
 	}
 	/////
 
@@ -151,44 +153,120 @@ public class A3 {
 	 * output file has the correct format and ordering
 	 */
 	public void printResult() {
+		Token tk = new Token(null);
 		System.out.println("Total Words: " + wordCount);
 		System.out.println("Unique Words: " + wordsByNaturalOrder.size());
 		System.out.println("Stop Words: " + stopWordCount + "\n");
 
 		System.out.println("10 Most Frequent");
 		
-		
-		
+		int count = 0;
 		Iterator<Token> itrMostReq = wordsByFreqDesc.iterator();
-		while(itrMostReq.hasNext()){
+		while(itrMostReq.hasNext() && count < 10){
 			System.out.println(itrMostReq.next().format());
+			count++;
 		}
 		System.out.println();
 		
 		
-		
 		System.out.println("10 Longest");
 		
-		
+		int count2 = 0;
 		Iterator<Token> itrMostLen = wordsByLength.iterator();
-		while(itrMostLen.hasNext()) {
-			System.out.println(itrMostLen.next().format());
+		while(itrMostLen.hasNext() && count2 < 10) {
+			if (count2 == 0) {
+				tk = itrMostLen.next();
+				System.out.println(tk.format());
+				count2++;
+			} else {
+				System.out.println(itrMostLen.next().format());
+				count2++;
+			}
 		}
 		
+		System.out.println();
 		
-		System.out.println("The longest word is ");
+		System.out.println("The longest word is " + tk.format());
 		System.out.println("The average word length is " + avgLength() + "\n");
 
 		System.out.println("All");
-
+		
+		Iterator<Token> bstIterator = wordsByNaturalOrder.iterator();
+		while (bstIterator.hasNext()) {
+			System.out.println(bstIterator.next().format());
+		}
+		
 		System.out.println();
 
 		System.out.println("Alphabetic Tree: (Optimum Height: " + optHeight(wordsByNaturalOrder.size())
 				+ ") (Actual Height: " + wordsByNaturalOrder.height() + ")");
 		
 		
-		System.out.println("Frequency Tree:" + "Optimum Height: " + optHeight(wordsByFreqDesc.size()) + " " + "Actual Height: " + wordsByFreqDesc.size());
-		System.out.println("Length Tree: (Optimum Height: 5) (Actual Height: 17)");
+		System.out.println("Frequency Tree: (Optimum Height: " + optHeight(wordsByFreqDesc.size()) 
+				+ ") (Actual Height: " + wordsByFreqDesc.height() + ")");
+		
+		System.out.println("Length Tree: (Optimum Height: " + optHeight(wordsByLength.size()) 
+				+ ") (Actual Height: " + wordsByLength.height() + ")");
+	}
+	
+	public void saveResult() throws FileNotFoundException {
+		Token tk = new Token(null);
+		PrintWriter outputFile = new PrintWriter("res/output.txt");
+		outputFile.println("Total Words: " + wordCount);
+		outputFile.println("Unique Words: " + wordsByNaturalOrder.size());
+		outputFile.println("Stop Words: " + stopWordCount + "\n");
+
+		outputFile.println("10 Most Frequent");
+		
+		int count = 0;
+		Iterator<Token> itrMostReq = wordsByFreqDesc.iterator();
+		while(itrMostReq.hasNext() && count < 10){
+			outputFile.println(itrMostReq.next().format());
+			count++;
+		}
+		outputFile.println();
+		
+		
+		outputFile.println("10 Longest");
+		
+		int count2 = 0;
+		Iterator<Token> itrMostLen = wordsByLength.iterator();
+		while(itrMostLen.hasNext() && count2 < 10) {
+			if (count2 == 0) {
+				tk = itrMostLen.next();
+				outputFile.println(tk.format());
+				count2++;
+			} else {
+				outputFile.println(itrMostLen.next().format());
+				count2++;
+			}
+		}
+		
+		outputFile.println();
+		
+		outputFile.println("The longest word is " + tk.format());
+		outputFile.println("The average word length is " + avgLength() + "\n");
+
+		outputFile.println("All");
+		
+		Iterator<Token> bstIterator = wordsByNaturalOrder.iterator();
+		while (bstIterator.hasNext()) {
+			outputFile.println(bstIterator.next().format());
+		}
+		
+		outputFile.println();
+
+		outputFile.println("Alphabetic Tree: (Optimum Height: " + optHeight(wordsByNaturalOrder.size())
+				+ ") (Actual Height: " + wordsByNaturalOrder.height() + ")");
+		
+		
+		outputFile.println("Frequency Tree: (Optimum Height: " + optHeight(wordsByFreqDesc.size()) 
+				+ ") (Actual Height: " + wordsByFreqDesc.height() + ")");
+		
+		outputFile.println("Length Tree: (Optimum Height: " + optHeight(wordsByLength.size()) 
+				+ ") (Actual Height: " + wordsByLength.height() + ")");
+		
+		outputFile.close();
 	}
 
 	/**
@@ -197,6 +275,7 @@ public class A3 {
 	public static void main(String[] args) throws Exception {
 		A3 a3 = new A3();
 		a3.readFile();
-        a3.printResult();
+        //a3.printResult();
+		a3.saveResult();
 	}
 }
